2a8fac05fabc0ded35b6f01b8fb0ef8220c6a1d0
David Rubert <david.rubert@gmail.com>
2013-07-02 23:34:14 +0200

    changes

--- app/js/main.js
+++ app/js/main.js
@@ -1,18 +1,18 @@
-var ctx = Sketch.create({
-    container: document.getElementById( 'canvas' ),
-    autoclear: false,
-    fullscreen: true,
-    type: Sketch.CANVAS,
-    globals: true,
-    autostart: true,
-    autopause: true,
-    interval: 1
+// create a wrapper around native canvas element (with id="c")
+
+var currCanvas = document.getElementById('c');
+var currCtx = currCanvas.getContext('2d');
+var viewportWidth = window.innerWidth;
+var viewportHeight = window.innerHeight;
+currCanvas.style.position = "fixed";
+currCanvas.setAttribute("width", viewportWidth);
+currCanvas.setAttribute("height", viewportHeight);
+currCanvas.style.top = 0;
+currCanvas.style.left = 0;
+
+var canvas = new fabric.Canvas('c');
+
+fabric.Image.fromURL('images/assets/toast.jpg', function(img) {
+    img.scale(0.5);
+    canvas.add(img);
 });
-
-
-ctx.draw = function() {
-    console.log(ctx.height, ctx.width);
-    ctx.beginPath();
-    ctx.arc( random( ctx.width ), random( ctx.height ), 10, 0, TWO_PI );
-    ctx.fill();
-}
