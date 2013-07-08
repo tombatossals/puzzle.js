97ee333997b2330da00752723a3b86f9f6641cc5
David Rubert <david.rubert@gmail.com>
2013-07-08 08:04:39 +0200

    assets

--- app/js/main.js
+++ app/js/main.js
@@ -11,8 +11,64 @@ currCanvas.style.top = 0;
 currCanvas.style.left = 0;
 
 var canvas = new fabric.Canvas('c');
+canvas.hoverCursor = 'pointer';
 
-fabric.Image.fromURL('images/assets/toast.jpg', function(img) {
-    img.scale(0.5);
+fabric.Image.fromURL('images/assets/toast.png', function(img) {
     canvas.add(img);
+    img.scaleToWidth(200);
+    canvas.centerObjectH(img).centerObjectV(img);
+    img.setCoords();
+    canvas.renderAll();
+});
+
+function animate(e, dir) {
+    if (e.target) {
+        fabric.util.animate({
+            startValue: e.target.get('angle'),
+            endValue: e.target.get('angle') + (dir ? 10 : -10),
+            duration: 100,
+            onChange: function(value) {
+                e.target.setAngle(value);
+                canvas.renderAll();
+            },
+            onComplete: function() {
+                e.target.setCoords();
+            }
+        });
+        fabric.util.animate({
+            startValue: e.target.get('scaleX'),
+            endValue: e.target.get('scaleX') + (dir ? 0.2 : -0.2),
+            duration: 100,
+            onChange: function(value) {
+                e.target.scale(value);
+                canvas.renderAll();
+            },
+            onComplete: function() {
+                e.target.setCoords();
+            }
+        });
+    }
+}
+
+canvas.on({
+    'mouse:down': function(e) {
+        if (e.target) {
+            animate(e, 1);
+            e.target.opacity = 0.5;
+            canvas.renderAll();
+        }
+    },
+    'mouse:up': function(e) {
+        if (e.target) {
+            animate(e, 0);
+            e.target.opacity = 1;
+            canvas.renderAll();
+        }
+    },
+    'object:moved': function(e) {
+        e.target.opacity = 0.5;
+    },
+    'object:modified': function(e) {
+        e.target.opacity = 1;
+    }
 });
